e024abb77fab089d19ec9741b95db436731126f1
Hugo Carvalho <webnoico1978@gmail.com>
2020-05-01 11:06:45 +0200

    <Header>: styled v.1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./Header.scss";
 
+//TODO: put background image - priority: very low
+//TODO: search icon for options (cog style)
+//TODO: see font weigth 500
+
 export default function Header() {
   return (
     <>
       <header>
-        <div className="options">Options</div>
-        <div className="wrapper-headers">
+        {/* <div className="options">Options</div> */}
+        <div className="wrapper__header">
           <h1>Weatherjetzt</h1>
-          <h3>Get your weather everywhere. Or almost...</h3>
+          <p>Get your weather everywhere. Or almost...</p>
         </div>
       </header>
     </>
   );
 }
-
-//TODO: put background image - priority: very low
-//TODO: search icon for options (cog style) - required
