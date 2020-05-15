7606a76de861e2c4d0250cfc95d7add301dc14f9
Hugo Carvalho <webnoico1978@gmail.com>
2020-05-15 20:18:26 +0200

    testing unhandledrejection

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ export const ErrorMsgContext = React.createContext();
 export const ShowErrorContext = React.createContext();
 export const IsNightContext = React.createContext();
 
+//TODO: check focus behaviour
+//TODO!!!: check bug when choosing a city in fast track after displaying a dif day
+//TODO: check hoover effect on cities fast track
+
 function App() {
   const key = "82005d27a116c2880c8f0fcb866998a0";
   // prettier-ignore
@@ -64,6 +68,11 @@ function App() {
         setShowError(true);
       });
   }, [isLoading, city, key]);
+
+  window.addEventListener(
+    "unhandledrejection",
+    alert("An error has ocurred, please reload ")
+  );
 
   return (
     <>
