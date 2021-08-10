2fbfff991101b8c18c0ab8c1656cf90784511988
Hugo Carvalho <webnoico1978@gmail.com>
2021-08-10 17:59:57 +0200

    add first test

--- src/components/InputSearchCity/InputSearchCity.test.js
+++ src/components/InputSearchCity/InputSearchCity.test.js
@@ -1,19 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import InputSearchCity from "./InputSearchCity";
 import App from "../../App";
 
 test("should render city searchbox and a go button ", () => {
-  let res;
-  const handleSubmit = (text) => (res = text);
   const Wrapper = ({ children }) => <App>{children}</App>;
   render(<InputSearchCity />, { wrapper: Wrapper });
-  // render(
-  //   <App onSubmit={handleSubmit}>
-  //     <InputSearchCity />
-  //   </App>
-  // );
 
   screen.getByPlaceholderText(/city name/i);
   screen.getByRole("heading", { name: /search for a city/i });
