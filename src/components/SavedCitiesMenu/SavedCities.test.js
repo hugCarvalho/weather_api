19995b24e5b5afc02361ef24245235f5976231e1
Hugo Carvalho <webnoico1978@gmail.com>
2021-08-11 18:21:29 +0200

    add first test

--- src/components/SavedCitiesMenu/SavedCities.test.js
+++ src/components/SavedCitiesMenu/SavedCities.test.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, container } from "@testing-library/react";
 import SavedCitiesMenu from "./SavedCitiesMenu";
 import App from "../../App";
 
-test("should ", () => {
+test("1st render and no saved cities should render correctly ", () => {
   render(
     <App>
       <SavedCitiesMenu />
     </App>
   );
+
+  const citiesBtn = screen.queryAllByRole("button", { name: /empty/i });
+  const saveBtn = document.body.querySelectorAll(".radio-btns");
+  expect(citiesBtn).toHaveLength(3);
+  expect(saveBtn.length).toBe(3);
 });
