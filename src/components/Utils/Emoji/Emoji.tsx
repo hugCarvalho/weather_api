7f507b47e78687e08e1aa0c3582b0bd7aa20c29e
Hugo Carvalho <webnoico1978@gmail.com>
2021-10-17 11:20:12 +0200

    added Emoji component

---
+++ src/components/Utils/Emoji/Emoji.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components"
+
+type EmojiProps = {
+  title: string,
+  emoji: string
+}
+
+const EmojiContainer = styled.span<EmojiProps>`
+  /* to prevent accessibility warnings */
+  color: black
+`
+
+export function Emoji({title, emoji}) {
+  return (
+    <EmojiContainer
+      role="img"
+      aria-label={title}
+      title={title}
+      emoji={emoji}
+    >
+      {emoji}
+    </EmojiContainer>
+  );
+}
+
+export default Emoji;
