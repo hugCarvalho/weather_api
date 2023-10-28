ed6ad1aff1ab5bf24787f5fd0d10a0f89cd18f5d
Hugo Carvalho <webnoico1978@gmail.com>
2023-10-28 23:10:31 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,101 +1,100 @@
-import React, { useState, useEffect, useReducer} from "react";
-import "./App.scss";
-import Header from "./components/Header/Header";
-import InputSearchCity from "./components/SearchCity/SearchCity";
-import DisplayErrorMsg from "./components/DisplayErrorMsg/DisplayErrorMsg";
-import SavedCitiesMenu from "./components/SavedCitiesMenu/SavedCitiesMenu";
-import DisplayCityName from "./components/DisplayCityName/DisplayCityName";
-import { errorInit, errorReducer } from "./components/reducers";
-import { InfoDaysAndTime } from "./components/InfoDaysAndTime/InfoDaysAndTime";
+import React, { useState, useEffect, useReducer } from 'react'
+import './App.scss'
+import Header from './components/Header/Header'
+import InputSearchCity from './components/SearchCity/SearchCity'
+import DisplayErrorMsg from './components/DisplayErrorMsg/DisplayErrorMsg'
+import SavedCitiesMenu from './components/SavedCitiesMenu/SavedCitiesMenu'
+import DisplayCityName from './components/DisplayCityName/DisplayCityName'
+import { errorInit, errorReducer } from './components/reducers'
+import { InfoDaysAndTime } from './components/InfoDaysAndTime/InfoDaysAndTime'
 
-export const UserQueryContext = React.createContext();
-export const ErrorContext = React.createContext();
-export const IsNightContext = React.createContext();
+export const UserQueryContext = React.createContext()
+export const ErrorContext = React.createContext()
+export const IsNightContext = React.createContext()
 
 //TODO: move optionsConfig and split types props
 function App() {
-  const key = "f2b65d46e479364d7c9f2127abfcb2b4";
-  const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState(null);
-  const [userQuery, setUserQuery] = useState("");
-  const [validCity, setValidCity] = useState(""); //is useful when searching for an invalid city if there's no city saved yet
-  const [isNight, setIsNight] = useState(false);
-  const [error, dispatchError] = useReducer(errorReducer, errorInit);
-  const [forecast3Days, setForecast3Days] = useState({});
+  const key = process.env.REACT_APP_API
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState(null)
+  const [userQuery, setUserQuery] = useState('')
+  const [validCity, setValidCity] = useState('') //is useful when searching for an invalid city if there's no city saved yet
+  const [isNight, setIsNight] = useState(false)
+  const [error, dispatchError] = useReducer(errorReducer, errorInit)
+  const [forecast3Days, setForecast3Days] = useState({})
 
+  console.log('key', key)
   //FETCH DATA
   useEffect(() => {
-    setIsLoading(true); //don't change
+    setIsLoading(true) //don't change
     const getWeather = async () => {
-      const api = `https://api.openweathermap.org/data/2.5/forecast?q=${userQuery}&appid=${key}`;
-      const response = await fetch(api);
-      const data = await response.json();
-      if (data.cod === "200") {
+      const api = `https://api.openweathermap.org/data/2.5/forecast?q=${userQuery}&appid=${key}`
+      const response = await fetch(api)
+      const data = await response.json()
+      if (data.cod === '200') {
         setData({
           weather: data,
-        });
-        setValidCity(userQuery);
-        setIsLoading(false);
+        })
+        setValidCity(userQuery)
+        setIsLoading(false)
       } else {
-        dispatchError({ type: "TRUE", value: data.message });
-        setIsLoading(false);
+        dispatchError({ type: 'TRUE', value: data.message })
+        setIsLoading(false)
       }
-    };
+    }
 
     userQuery &&
-      getWeather().catch(e => {
-        dispatchError({ type: "TRUE", value: "Something went wrong..." });
-      });
-  }, [userQuery, key]);
+      getWeather().catch((e) => {
+        dispatchError({ type: 'TRUE', value: 'Something went wrong...' })
+      })
+  }, [userQuery, key])
 
   //sets forecast for 3 days
   useEffect(() => {
-    let today = [];
-    let tomorrow = [];
-    let afterTomorrow = [];
-    let dayIs = null;
-    let control = 0;
+    let today = []
+    let tomorrow = []
+    let afterTomorrow = []
+    let dayIs = null
+    let control = 0
 
     if (data) {
       data.weather.list.forEach((dayObj, i) => {
-        const day = +dayObj.dt_txt.slice(8, 10);
+        const day = +dayObj.dt_txt.slice(8, 10)
         if (i === 0) {
-          dayIs = day;
-          control += 1;
+          dayIs = day
+          control += 1
         }
         if (dayIs !== day) {
-          dayIs = day;
-          control += 1;
+          dayIs = day
+          control += 1
         }
         if (control === 1) {
-          today.push(dayObj);
-          return;
+          today.push(dayObj)
+          return
         }
         if (control === 2) {
-          tomorrow.push(dayObj);
-          return;
+          tomorrow.push(dayObj)
+          return
         }
         if (control === 3) {
-          afterTomorrow.push(dayObj);
-          return;
+          afterTomorrow.push(dayObj)
+          return
         }
-        return;
-      });
+        return
+      })
     }
     setForecast3Days({
       today,
       tomorrow,
       afterTomorrow,
-    });
-
-  }, [data]);
+    })
+  }, [data])
 
   return (
     <>
       <div
-        className="container__app"
-        style={isNight ? { background: "#202020" } : { background: "#7cafeb" }}
-      >
+        className='container__app'
+        style={isNight ? { background: '#202020' } : { background: '#7cafeb' }}>
         <Header />
         <ErrorContext.Provider value={{ error, dispatchError }}>
           <UserQueryContext.Provider value={{ userQuery, setUserQuery }}>
@@ -103,8 +102,11 @@ function App() {
             <DisplayErrorMsg />
             <SavedCitiesMenu validCity={validCity} />
           </UserQueryContext.Provider>
-        </ErrorContext.Provider>{" "}
-        <DisplayCityName validCity={validCity} isLoading={isLoading} />
+        </ErrorContext.Provider>{' '}
+        <DisplayCityName
+          validCity={validCity}
+          isLoading={isLoading}
+        />
         <IsNightContext.Provider value={{ isNight, setIsNight }}>
           <InfoDaysAndTime
             data={data}
@@ -115,6 +117,6 @@ function App() {
         </IsNightContext.Provider>
       </div>
     </>
-  );
+  )
 }
-export default App;
+export default App
