e67c562e52a7616eccdcce1942d223bfdcc35717
Hugo Carvalho <webnoico1978@gmail.com>
2023-10-31 08:55:54 +0100

    .

--- src/components/DisplayCityName/DisplayCityName.js
+++ src/components/DisplayCityName/DisplayCityName.js
@@ -1,25 +1,34 @@
-import React from "react";
-import "./DisplayCityName.scss";
-import { ClipLoader } from "react-spinners";
-import PropTypes from "prop-types";
+import React from 'react'
+import './DisplayCityName.scss'
+import { ClipLoader } from 'react-spinners'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const CityName = styled.h2`
+  padding: 15px 3px 20px;
+  text-align: center;
+  color: #faf9f9;
+  letter-spacing: 2px;
+  font-size: 20px;
+
+  @media (min-width: 768px) {
+    font-size: 26px;
+  }
+`
 
 export default function DisplayCityName({ validCity, isLoading }) {
   return (
     <>
-      <header className="container__city-name">
+      <CityName>
         <h2>
-          {isLoading && validCity ? (
-            <ClipLoader color="white" />
-          ) : (
-            validCity.toUpperCase() || "Search for a city"
-          )}
+          {isLoading && validCity ? <ClipLoader color='white' /> : validCity.toUpperCase() || 'Search for a city'}
         </h2>
-      </header>
+      </CityName>
     </>
-  );
+  )
 }
 
 DisplayCityName.propTypes = {
   validCity: PropTypes.string,
   isLoading: PropTypes.bool,
-};
+}
